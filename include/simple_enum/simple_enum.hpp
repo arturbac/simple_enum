35988bb82ae635b1f32b0ad59709e219ddd948af
Artur Bać <artur@ebasoft.com.pl>
2024-03-03 21:26:10 +0100

    v0.5.0

--- include/simple_enum/simple_enum.hpp
+++ include/simple_enum/simple_enum.hpp
@@ -10,7 +10,7 @@
 #endif
 #include <array>
 
-#define SIMPLE_ENUM_NAME_VERSION "0.4.2"
+#define SIMPLE_ENUM_NAME_VERSION "0.5.0"
 
 #pragma push_macro("static_constexpr")
 #pragma push_macro("static_call_operator")
