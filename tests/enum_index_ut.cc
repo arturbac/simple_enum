264f88b403ec191944f064aa106f69dddbef4990
Artur Bać <artur@ebasoft.com.pl>
2024-03-04 23:10:01 +0100

    make std_format_ut run with most modern compilers where format is well implemented

--- tests/enum_index_ut.cc
+++ tests/enum_index_ut.cc
@@ -9,7 +9,7 @@ int main()
   "enum_index valid"_test = []
   {
     using simple_enum::to_underlying;
-    // Test for valid enum values
+
     expect(eq(enum_index(lorem_ipsum_short::eu).value(), to_underlying(lorem_ipsum_short::eu)));
     expect(eq(enum_index(lorem_ipsum_short::adipiscing).value(), to_underlying(lorem_ipsum_short::adipiscing)));
     expect(eq(enum_index(lorem_ipsum_short::exercitation).value(), to_underlying(lorem_ipsum_short::exercitation)));
@@ -18,11 +18,8 @@ int main()
   "enum_index out_of_range"_test = []
   {
     using simple_enum::to_underlying;
-    // Assuming there's a way to construct an out-of-range value, e.g., using static_cast
     auto out_of_range_value = static_cast<lorem_ipsum_short>(to_underlying(lorem_ipsum_short::last) + 1);
     expect(enum_index(out_of_range_value).error() == enum_index_error::out_of_range);
-
-    // If you have a specific mechanism to test out-of-range values, adjust the above accordingly.
   };
   }
 
